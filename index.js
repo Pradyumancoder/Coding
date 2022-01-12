cfc68c960144df43feeb68f1db53c9ffd413d07f
Pradyuman Shukla <97114184+Pradyumancoder@users.noreply.github.com>
2022-01-12 11:07:17 -0800

    javascript and html and css
    in witch js & html both are included

---
+++ index.js
@@ -0,0 +1,11 @@
+ // this is my first javascript!
+var name ='pradyuman'
+console.log(name);
+let number ='90'
+console.log(number)
+let sqare ='2*2'
+console.log(sqare);
+let firstname ='Rizwan'
+let lastname ='Khan'
+console.log(firstname+' '+lastname);
+    
